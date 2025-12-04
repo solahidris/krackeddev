a406676e4279dc5d3383b8705909dd5ae7db1442
wannabil <nabil@wannabil.com>
2025-12-04 10:31:52 +0800

    fix: remove unused imports in onboarding page to pass linting

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { OnboardingTown } from "@/components/game/OnboardingTown";
 import "../jobs/jobs.css";
